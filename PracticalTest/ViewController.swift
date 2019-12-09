877cec14e3a5429d27eab329a6c4fec5336d6a25
nomz <s10187493@connect.np.edu.sg>
2019-12-09 11:50:31 +0800

    Initial Commit

---
+++ PracticalTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticalTest
+//
+//  Created by MAD2 on 9/12/19.
+//  Copyright © 2019 MAD2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
